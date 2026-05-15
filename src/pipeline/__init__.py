5b5ab27b1c214622b4d65ad74724ab71169de3e7
ishithabusetty <148688617+ishithabusetty@users.noreply.github.com>
2026-05-15 22:38:03 +0530

    Delete src directory

--- src/pipeline/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/pipeline/__init__.py
-"""Pipeline modules for unified detection and decision making."""
-
-from .unified_pipeline import UnifiedPipeline
-from .decision_engine import DecisionEngine
-from .video_processor import VideoProcessor
-
-__all__ = ['UnifiedPipeline', 'DecisionEngine', 'VideoProcessor']
